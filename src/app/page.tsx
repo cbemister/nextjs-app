c54fdbdd9a23d0607ee2bffa6e8655038e5445d5
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2025-01-03 02:24:40 -0700

    Web hook works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Hello World</h1>
         <p>Let's see if this works ... again.</p>
+        <p>Success!!!</p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
